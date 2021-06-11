5a704d696660168fba8339ee9ab9167dc4342244
grandiosa iva <ggo24@yahoo.com>
2021-06-11 11:49:03 +0400

    chapter 10 part 4

---
+++ Chapter_10_Views/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace Chapter_10_Views.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
