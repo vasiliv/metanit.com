26ef1088ebee56d90c2d6d277fe6e8941192b6ff
grandiosa iva <ggo24@yahoo.com>
2021-08-07 19:13:57 +0400

    IdentityApp FInished

--- IdentityApp/Controllers/AdminController.cs
+++ IdentityApp/Controllers/AdminController.cs
@@ -1,6 +1,7 @@
 ﻿using IdentityApp.Data;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -10,24 +11,31 @@ namespace IdentityApp.Controllers
 {
     public class AdminController : Controller
     {
-        //ApplicationDbContext _context;
-        //public AdminController(ApplicationDbContext context)
-        //{
-        //    _context = context;
-        //}
+        ApplicationDbContext _context;
+        UserManager<IdentityUser> _userManager;
+        RoleManager<IdentityRole> _roleManager;
+        public AdminController(/*UserManager<IdentityUser> manager,*/ ApplicationDbContext context, RoleManager<IdentityRole> manager)
+        {
+            //_userManager = manager;
+            _context = context;
+            _roleManager = manager;
+        }        
         //public IActionResult Index()
         //{
         //    var users = _context.Users.ToList();
         //    return View(users);
-        //}
-        UserManager<IdentityUser> _userManager;
-        public AdminController(UserManager<IdentityUser> manager)
-        {
-            _userManager = manager;
-        }
+        //}         
         public IActionResult Index()
         {
             return View(_userManager.Users.ToList());
+        }
+        public async Task<IActionResult> GetRoles()
+        {
+            //_context.Add(new IdentityRole("simpleUser"));
+            //await _context.SaveChangesAsync();
+            //return View(await _context.Roles.ToListAsync());
+            await _roleManager.CreateAsync(new IdentityRole { Name = "Admin", NormalizedName = "ADMIN" });
+            return View(await _roleManager.Roles.ToListAsync());
         }
     }
 }
--- IdentityApp/Startup.cs
+++ IdentityApp/Startup.cs
@@ -32,7 +32,8 @@ namespace IdentityApp
                     Configuration.GetConnectionString("DefaultConnection")));
             services.AddDatabaseDeveloperPageExceptionFilter();
 
-            services.AddDefaultIdentity<IdentityUser>(options => options.SignIn.RequireConfirmedAccount = true)
+            //services.AddDefaultIdentity<IdentityUser>(options => options.SignIn.RequireConfirmedAccount = true)
+            services.AddIdentity<IdentityUser, IdentityRole>(options => options.SignIn.RequireConfirmedAccount = true)
                 .AddEntityFrameworkStores<ApplicationDbContext>();
             services.AddControllersWithViews();
         }
